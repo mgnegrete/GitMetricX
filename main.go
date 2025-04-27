85fa36617dc767e3036529a757322630ffcfd479
mgnegrete <mgnegrete24@gmail.com>
2025-04-26 22:26:22 -0700

    added commit count

--- main.go
+++ main.go
@@ -3,7 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+var (
+	commitCount = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "gitmetricx_commits_total",
+			Help: "Total number of commits in the repository",
+		},
+		[]string{"repo"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(commitCount)
+}
 
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "# HELP gitmetricx_commits_total Total number of commits")
