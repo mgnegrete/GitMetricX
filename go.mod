9e8a0a22e25144c19951b07a0716adf02c02721c
mgnegrete <mgnegrete24@gmail.com>
2025-04-26 22:15:04 -0700

    updated dependencies

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mgnegrete24/GitMetricX
+
+go 1.24.2
--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "# HELP gitmetricx_commits_total Total number of commits")
-    fmt.Fprintln(w, "# TYPE gitmetricx_commits_total counter")
-    fmt.Fprintln(w, "gitmetricx_commits_total 42")
+	fmt.Fprintln(w, "# HELP gitmetricx_commits_total Total number of commits")
+	fmt.Fprintln(w, "# TYPE gitmetricx_commits_total counter")
+	fmt.Fprintln(w, "gitmetricx_commits_total 42")
 }
 
 func main() {
-    http.HandleFunc("/metrics", metricsHandler)
-    fmt.Println("Starting server on :8080")
-    http.ListenAndServe(":8080", nil)
-}&#8203;:contentReference[oaicite:14]{index=14}
+	http.HandleFunc("/metrics", metricsHandler)
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", nil)
+}
